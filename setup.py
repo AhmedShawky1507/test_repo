9765f7adddda82c094945b30906f6ccf487f96c4
Ahmed Shawky <72513905+AhmedShawky1507@users.noreply.github.com>
2025-06-13 22:08:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-print("Hello World!!!!!!!!!!!!!!!!!!!!")
+from setuptools import setup
+
+setup(
+    name="test_project",  # Required
+    version="0.1.0",     # Required
+    packages=[""],       # Include all Python files in current directory
+    # Optional metadata
+    author="Your Name",
+    description="Test package for CI/CD pipeline",
+    python_requires=">=3.6",
+)
+print("Hello world!!!!!!!!!!!!!!!!!!!!!")
